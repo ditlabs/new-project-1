e7cb289d9ac594dbf5e265fb3e574657434d7681
ditlabs <radityarizki2309@gmail.com>
2025-07-12 23:45:05 +0700

    implementasi fitur CRUD

---
+++ app/Filament/Resources/ProdukResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Produk;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Tabs\Tab;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProdukResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProdukResource\RelationManagers;
+
+class ProdukResource extends Resource
+{
+    protected static ?string $model = Produk::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Textinput::make('nama_produk')->required()->maxLength(50)->directory('gambar-produk'),
+                Textinput::make('harga_produk')->required()->numeric()->prefix('Rp'),
+                textarea::make('deskripsi_produk')->required()->maxLength(255)->columnSpanFull(),
+                FileUpload::make('gambar_produk')->image()->disk('public')->columnSpanFull()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama_produk')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('harga_produk')
+                    ->prefix('Rp')
+                    ->formatStateUsing(fn ($state) => number_format($state, 0, ',','.'))
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProduks::route('/'),
+            'create' => Pages\CreateProduk::route('/create'),
+            'edit' => Pages\EditProduk::route('/{record}/edit'),
+        ];
+    }
+}
--- database/migrations/2025_07_12_154014_create_produks_table.php
+++ database/migrations/2025_07_12_154014_create_produks_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_produk');
+            $table->text('deskripsi_produk');
+            $table->integer('harga_produk');
+            $table->string('gambar_produk');
+            $table->timestamps();
         });
     }
 
@@ -21,8 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('produks');
     }
 };
